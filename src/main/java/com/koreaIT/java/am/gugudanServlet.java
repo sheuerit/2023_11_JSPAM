feab00572db1ef9f97df05ab70fbd949f519b024
sheuerit <sheuerit@gmail.com>
2023-11-03 17:12:04 +0900

    servlet practice, parameter practice

---
+++ src/main/java/com/koreaIT/java/am/gugudanServlet.java
@@ -0,0 +1,20 @@
+package com.koreaIT.java.am;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/home/gugudan")
+public class gugudanServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("== 8단 ==\n").append("8 * 1 = 8\n");
+	}
+
+}
